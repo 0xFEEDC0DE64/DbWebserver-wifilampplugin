1d9bfba588fbd784d4829c04348f7c35d7ebe312
0xFEEDC0DE64 <0xFEEDC0DE64@gmail.com>
2018-09-22 03:07:20 +0200

    Implemented basic example response

--- wifilampapplication.cpp
+++ wifilampapplication.cpp
@@ -1,5 +1,9 @@
 #include "wifilampapplication.h"
 
+#include "httprequest.h"
+#include "httpresponse.h"
+#include "httpclientconnection.h"
+
 WifiLampApplication::WifiLampApplication(const QJsonObject &config, QObject *parent) :
     WebApplication(parent)
 {
@@ -9,4 +13,12 @@ WifiLampApplication::WifiLampApplication(const QJsonObject &config, QObject *par
 void WifiLampApplication::start()
 {
 
+}
+
+void WifiLampApplication::handleRequest(HttpClientConnection *connection, const HttpRequest &request)
+{
+    HttpResponse response;
+    response.protocol = request.protocol;
+    response.statusCode = HttpResponse::StatusCode::OK;
+    connection->sendResponse(response, "Hello from WifiLampApplication: " + request.path);
 }
--- wifilampapplication.h
+++ wifilampapplication.h
@@ -12,4 +12,6 @@ public:
     WifiLampApplication(const QJsonObject &config, QObject *parent = Q_NULLPTR);
 
     void start() Q_DECL_OVERRIDE;
+
+    void handleRequest(HttpClientConnection *connection, const HttpRequest &request) Q_DECL_OVERRIDE;
 };
